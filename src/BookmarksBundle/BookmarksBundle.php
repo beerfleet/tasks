628226268c87d619aceb92cb8c2b6281a04a7327
Jakke <jvbiervliet@gmail.com>
2017-08-08 16:34:13 +0200

    created BookmarksBundle

---
+++ src/BookmarksBundle/BookmarksBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace BookmarksBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Description of BookmarksBundle
+ *
+ * @author jvanbiervliet
+ */
+class BookmarksBundle extends Bundle {
+  //put your code here
+}
---
+++ src/BookmarksBundle/Controller/HomepageController.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace BookmarksBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Description of HomepageController
+ *
+ * @author jvanbiervliet
+ */
+class HomepageController extends Controller {
+
+  public function dashboardAction() {
+    
+  }
+
+}
