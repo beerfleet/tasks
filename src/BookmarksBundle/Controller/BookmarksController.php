5d8d1d4c68d9de32519cd838567050d4b0a390bd
Jakke <jvbiervliet@gmail.com>
2017-08-11 11:31:49 +0200

    charset

---
+++ src/BookmarksBundle/Controller/BookmarksController.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace BookmarksBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Description of BookmarksController
+ *
+ * @author jvanbiervliet
+ */
+class BookmarksController extends Controller {
+
+  public function showBookmarksAction() {
+    
+  }
+
+}
