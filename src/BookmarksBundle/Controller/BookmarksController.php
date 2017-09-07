62253a75b712076eeaafdedd655b0f0de1c716df
Jakke <jvbiervliet@gmail.com>
2017-09-07 09:41:35 +0200

    Nieuwe bookmark input form klaar TODO: verwerking

--- src/BookmarksBundle/Controller/BookmarksController.php
+++ src/BookmarksBundle/Controller/BookmarksController.php
@@ -8,7 +8,11 @@
 
 namespace BookmarksBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use BookmarksBundle\Entity\Bookmark;
 /**
  * Description of BookmarksController
  *
@@ -24,7 +28,20 @@ class BookmarksController extends Controller {
   }
   
   public function addNewBookmarkAction() {
+    $srv = $this->get('bookmarks.service');
+    $bookmark = new Bookmark();
+    
     return $this->render('@Bookmarks/Bookmarks/bookmark-add-new.html.twig', []);
+  }
+  
+  public function storeBookmarkAction() {
+    /* @var BookmarksService $srv */
+    $srv = $this->get('bookmarks.service');
+    $status = $srv->storeBookmarks();
+    if ($status == 0) {
+      throw new \Exception("FAILED");
+    }
+    return new \Symfony\Component\HttpFoundation\Response("<h1>TIS GELUKT</h1>");
   }
 
 }
