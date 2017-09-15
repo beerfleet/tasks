1247aec0e458698a37a86bacd5bb019d6acea13e
Jakke <jvbiervliet@gmail.com>
2017-09-15 16:11:06 +0200

    store bookmark almost done

--- src/BookmarksBundle/Controller/BookmarksController.php
+++ src/BookmarksBundle/Controller/BookmarksController.php
@@ -8,40 +8,35 @@
 
 namespace BookmarksBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use BookmarksBundle\Entity\Bookmark;
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * Description of BookmarksController
  *
  * @author jvanbiervliet
  */
+class BookmarksController extends Controller {
 
-class BookmarksController extends Controller {    
-  
-  public function showBookmarksAction() {    
+  public function showBookmarksAction() {
     $srv = $this->get('bookmarks.service');
     $bookmarks = $srv->fetchAllBookmarks();
     return $this->render('@Bookmarks/Bookmarks/bookmarks.html.twig', ['bookmarks' => $bookmarks]);
   }
-  
-  public function addNewBookmarkAction() {
-    $srv = $this->get('bookmarks.service');
-    $bookmark = new Bookmark();
-    
+
+  public function addNewBookmarkAction() {    
     return $this->render('@Bookmarks/Bookmarks/bookmark-add-new.html.twig', []);
   }
-  
+
   public function storeBookmarkAction() {
-    /* @var BookmarksService $srv */
     $srv = $this->get('bookmarks.service');
-    $status = $srv->storeBookmarks();
-    if ($status == 0) {
-      throw new \Exception("FAILED");
-    }
-    return new \Symfony\Component\HttpFoundation\Response("<h1>TIS GELUKT</h1>");
+    $status = $srv->storeBookmark();
+    /* @var $status \Exception */
+    if ($status !== 1) {
+      $this->addFlash('error', $status);
+      return $this->redirectToRoute('bookmarks-add-new');
+    }    
+    return new Response("<h1>TIS GELUKT</h1>");
   }
 
 }
--- src/BookmarksBundle/Service/BookmarksService.php
+++ src/BookmarksBundle/Service/BookmarksService.php
@@ -13,23 +13,62 @@ namespace BookmarksBundle\Service;
  *
  * @author jvanbiervliet
  */
-
 use BookmarksBundle\Entity\Bookmark;
 use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Doctrine\DBAL\Exception\DriverException;
 
 class BookmarksService {
-  
+
   private $em;
-  
-  public function __construct(EntityManager $em) {
-    $this->em = $em;
+  private $rs;
+
+  public function __construct(EntityManager $entity_manager, RequestStack $request_stack) {
+    $this->em = $entity_manager;
+    $this->rs = $request_stack;
   }
 
   public function fetchAllBookmarks() {
-    /** @var $repo EntityRepository */    
-    $repo = $this->em->getRepository(Bookmark::class);    
+    /** @var $repo EntityRepository */
+    $repo = $this->em->getRepository(Bookmark::class);
     $bookmarks = $repo->findAllSorted();
     return $bookmarks;
+  }
+
+  public function storeBookmark() {
+    $status = 0;
+    $bookmark = new Bookmark();
+    $request = $this->rs->getCurrentRequest();
+    $path = $request->get('path');
+    $title = $request->get('title');
+    $bookmark->setPath($path);
+    $bookmark->setTitle($title);
+    return $this->persist_object($bookmark);
+  }
+
+  public function persist_object($object) {
+    try {
+      $this->em->persist($object);
+      $this->em->flush();
+    } catch (DriverException $e) {
+      $error = $e->getErrorCode();
+      switch ($error) {
+        case 1062:
+          $message = "De opgegeven titel bestaat al.";
+          break;
+        case 1146:
+          $message = "Onbestaande tabel verwijzing.";
+          break;
+        case 2002:
+          $message = "De gegegevensbron is niet bereikbaar.";
+          break;
+      }
+      return $message;
+    } catch (\Exception $e) {
+      return 'Er is een ondekend probleem opgetreden.';
+    }
+
+    return 1;
   }
 
 }
